f488256c1373647761621576a19880c825f5c13b
tsuipz <tsuipz@gmail.com>
2025-06-22 16:07:44 -0400

    feat: new mock summary

--- src/app/modules/home/components/calorie-summary/calorie-summary.component.ts
+++ src/app/modules/home/components/calorie-summary/calorie-summary.component.ts
@@ -1,8 +1,27 @@
+import { CommonModule, DecimalPipe, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-calorie-summary',
-  template: `<p>calorie-summary works!</p>`,
+  templateUrl: './calorie-summary.component.html',
+  styleUrls: ['./calorie-summary.component.scss'],
   standalone: true,
+  imports: [NgStyle, DecimalPipe, CommonModule],
 })
-export class CalorieSummaryComponent {}
+export class CalorieSummaryComponent {
+  consumed = 950;
+  burned = 400;
+  target = 1650;
+
+  get net(): number {
+    return this.consumed - this.burned;
+  }
+
+  get remaining(): number {
+    return this.target - this.net;
+  }
+
+  get progress(): number {
+    return (this.net / this.target) * 100;
+  }
+}
