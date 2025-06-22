adbcbe4a3933774aeac4d5f8ac9eb8e434525254
Patrick Tsui <tsuipz@gmail.com>
2025-06-22 16:45:21 -0400

    21: create meals logged table (#23)
    * 21: create meals logged table
    
    * update: category pass

--- src/app/modules/home/components/meals-logged/meals-logged.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-meals-logged',
-  template: `<p>meals-logged works!</p>`,
-  standalone: true,
-})
-export class MealsLoggedComponent {}
