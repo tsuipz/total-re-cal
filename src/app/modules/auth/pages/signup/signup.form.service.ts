918fbbf468ffdf484e25a66f6c0821569f388a57
tsuipz <tsuipz@gmail.com>
2025-06-22 13:27:44 -0400

    11: create onboarding check in day step

--- src/app/modules/auth/pages/signup/signup.form.service.ts
+++ src/app/modules/auth/pages/signup/signup.form.service.ts
@@ -5,13 +5,27 @@ import {
   ProfileFormService,
   ProfileFormValue,
 } from './profile/profile.form.service';
+import {
+  PlanForm,
+  PlanFormService,
+  PlanFormValue,
+} from './plan/plan.form.service';
+import {
+  CheckInForm,
+  CheckInFormService,
+  CheckInFormValue,
+} from './check-in/check-in.form.service';
 
 export interface SignupFormValue {
   profile: ProfileFormValue;
+  plan: PlanFormValue;
+  checkIn: CheckInFormValue;
 }
 
 export interface SignupFormControls {
   profile: ProfileForm;
+  plan: PlanForm;
+  checkIn: CheckInForm;
 }
 
 export type SignupForm = FormGroup<SignupFormControls>;
@@ -20,12 +34,24 @@ export type SignupForm = FormGroup<SignupFormControls>;
 export class SignupFormService {
   private fb = inject(FormBuilder);
   private profileFormService = inject(ProfileFormService);
+  private planFormService = inject(PlanFormService);
+  private checkInFormService = inject(CheckInFormService);
 
   private signupForm: SignupForm = this.fb.group<SignupFormControls>({
     profile: this.profileFormService.form,
+    plan: this.planFormService.form,
+    checkIn: this.checkInFormService.form,
   });
 
   public get form(): SignupForm {
     return this.signupForm;
+  }
+
+  public get value(): SignupFormValue {
+    return this.signupForm.value as SignupFormValue;
+  }
+
+  public get isValid(): boolean {
+    return this.signupForm.valid;
   }
 }
