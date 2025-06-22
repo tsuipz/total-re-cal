3abf5e3ec8a1a8f4d5b4f238196f6296b7ac6b62
tsuipz <tsuipz@gmail.com>
2025-06-22 00:48:24 -0400

    fix: unit test

--- src/app/modules/auth/pages/signup/profile/profile.component.spec.ts
+++ src/app/modules/auth/pages/signup/profile/profile.component.spec.ts
@@ -1,16 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormControl, FormGroup } from '@angular/forms';
+import { ProfileFormService } from './profile.form.service';
 import { ProfileComponent } from './profile.component';
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
+  let mockProfileFormService: jest.Mocked<ProfileFormService>;
 
   beforeEach(async () => {
+    mockProfileFormService = {
+      form: new FormGroup({
+        name: new FormControl(''),
+        gender: new FormControl(''),
+        age: new FormControl(''),
+        height: new FormControl(''),
+        currentWeight: new FormControl(''),
+        unitSystem: new FormControl(''),
+      }),
+    } as unknown as jest.Mocked<ProfileFormService>;
+
     await TestBed.configureTestingModule({
-      imports: [ProfileComponent]
-    })
-    .compileComponents();
+      providers: [
+        { provide: ProfileFormService, useValue: mockProfileFormService },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ProfileComponent);
     component = fixture.componentInstance;
